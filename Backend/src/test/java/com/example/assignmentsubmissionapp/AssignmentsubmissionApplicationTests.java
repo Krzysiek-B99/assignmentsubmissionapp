1a51dcb5fc770cc9d972b9e23c5daa4c35dd6fe8
Krzysztof Bujak <krzysiek.bujak99@gmail.com>
2022-05-31 16:48:31 +0200

    Backend v1: Security, JWT | ep. 8

---
+++ Backend/src/test/java/com/example/assignmentsubmissionapp/AssignmentsubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentsubmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
