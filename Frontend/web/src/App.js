121889d9be0d3d35e2454e734426e52c1c30a73e
Krzysiek-B99 <krzysiek.bujak99@gmail.com>
2022-05-31 19:28:33 +0200

    CORS error, fetch:POST | ep.10

--- Frontend/web/src/App.js
+++ Frontend/web/src/App.js
@@ -2,6 +2,26 @@ import './App.css';
 import React from 'react'
 
 function App() {
+
+  const reqBody = {
+    "username":"test",
+    "password":"test"
+  }
+  fetch("api/auth/login",{
+    headers:{
+      "Content-Type":"application/json",
+
+    },
+    method:"post",
+    body:JSON.stringify(reqBody)
+  })
+  .then((response) => Promise.all([response.json(),response.headers]))
+  .then(([body,headers]) => {
+    const authValue = headers.get("authorization");
+    console.log(authValue);
+    console.log(body);
+  });
+
   return (
     <div className="App">
       <h1>Hello world</h1>
